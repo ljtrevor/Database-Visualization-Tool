a6e7d00bcba64edb59295c4283e78286009f3010
ljtrevor <ljtrevor0689@gmail.com>
2017-01-28 21:54:49 -0800

    account system functionality finished

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,21 @@ router.get('/', function(req, res){
 	res.render('index');
 });
 
+
+
+
+router.get('/users/login', ensureAuthenticated, function(req, res){
+	res.render('login');
+});
+
+function ensureAuthenticated(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	} else {
+		//req.flash('error_msg','You are not logged in');
+		res.redirect('/');
+	}
+}
+
+
 module.exports = router;
